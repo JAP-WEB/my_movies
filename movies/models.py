faa09799a1ea7f737b559988f005df484f396697
Denisse Abigail Guipzot Garibay <l20211788@tectijuana.edu.mx>
2024-04-11 21:52:07 +0000

    model update

--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Genre(models.Model):
      name = models.CharField(max_length=100)
      def __str__(self):
         return self.name
+        
+class Job(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    overview = models.TextField()
+    release_date = models.DateTimeField()
+    running_time = models.IntegerField()
+    budget = models.IntegerField(blank=True)
+    tmdb_id = models.IntegerField(blank=True, unique=True)
+    revenue = models.IntegerField(blank=True)
+    poster_path = models.URLField(blank=True)
+    genres = models.ManyToManyField(Genre)
+    credits = models.ManyToManyField(Person, through="MovieCredit")
+
+    def __str__(self):
+        return self.title
+
+
+class MovieCredit(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+
+
+class MovieReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1),
+                                                          MaxValueValidator(100)])
+    review = models.TextField(blank=True)
